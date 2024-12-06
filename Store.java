89f3c5aeef7feb6a3179972f7455fbae04fc5f07
dinhanh0 <anhcongdinh0711@gmail.com>
2024-12-06 16:01:52 -0500

    Create Store.java

---
+++ Store.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    private String name;
+    private double distance;
+    private List<String> items; // List to hold grocery items
+
+    // Constructor
+    public Store(String name, double distance) {
+        this.name = name;
+        this.distance = distance;
+        this.items = new ArrayList<>();
+    }
+}
+
+
